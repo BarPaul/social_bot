c73e8f72896874f729ecc784bc2774f66c4b646e
BarPaul <pav3l.bar4nas@gmail.com>
2024-01-29 21:26:59 +0300

    забыл запятую

--- logger.py
+++ logger.py
@@ -2,4 +2,4 @@ from logging import getLogger, basicConfig, DEBUG
 
 
 logger = getLogger(__name__)
-basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt="%d-%m-%Y %H:%M:%S" level=DEBUG)
+basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt="%d-%m-%Y %H:%M:%S", level=DEBUG)
