4f24fee74bac0afb584d1505fc94ca3e1d530853
BarPaul <pav3l.bar4nas@gmail.com>
2024-02-09 19:58:11 +0300

    logger level change

--- logger.py
+++ logger.py
@@ -1,5 +1,5 @@
-from logging import getLogger, basicConfig, DEBUG
+from logging import getLogger, basicConfig, INFO
 
 
 logger = getLogger(__name__)
-basicConfig(format='%(asctime)s %(levelname)s | %(message)s', datefmt="%d-%m-%Y %H:%M:%S", level=DEBUG)
+basicConfig(format='%(asctime)s %(levelname)s | %(message)s', datefmt="%d-%m-%Y %H:%M:%S", level=INFO)
